32eb687a3ea26633a0d11965f0607f91f404efb1
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2025-09-13 20:04:11 -0700

    update README

--- src/branchless_fizzbuzz.h
+++ src/branchless_fizzbuzz.h
@@ -1,11 +1,10 @@
 #ifndef BRANCHLESS_FIZZBUZZ_H
 #define BRANCHLESS_FIZZBUZZ_H
 
-// #include <stddef.h>   // ensure size_t is defined
 #include <stdint.h>   // ensure uint64_t is defined
 
-#define MIN_BUFFER_SIZE 30
-
 void bfb_fill_buffer(char*, uint64_t);
+
+#define MIN_BUFFER_SIZE 30
 
 #endif
--- src/driver.c
+++ src/driver.c
@@ -1,17 +1,19 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <stdint.h>
 
 #include "branchless_fizzbuzz.h"
 
 int main(int argc, char* argv[]) {
     char buffer[MIN_BUFFER_SIZE] = {0};
 
-    // could be unrolled and moved into the assembly (thereby eliminating a source of branches) if we're willing to hard
-    // code the loop bound, but for the sake of the binary's size we're not gonna do that.
     for (uint64_t i = 1; i != 1000; i++) {
         bfb_fill_buffer(buffer, i);
 
         puts(buffer);
     }
+
+    bfb_fill_buffer(buffer, UINT64_MAX / 10);
+    puts(buffer);
 }
